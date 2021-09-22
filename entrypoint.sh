652a5cbb1d33c6ab6624d9cb25b93889f8c1527b
LM <hi@leima.is>
2021-09-22 22:21:57 +0200

    just play around

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/sh -l
 
-echo "Hello $1"
+# $1:bib-path
+# $2:metadata-path
+# $3:connections-path
+# $4: map-path
+
+echo "Using bib path $1; metadata path: $2; connections path: $3; map path: $4"
 time=$(date)
-echo "::set-output name=time::$time"
-echo "$(kirsche)"
+echo "Starting at $time"
+echo "Calculating connections from bib file..."
+kirsche connections -sb $1 -c $3
+echo "Visualizing ..."
+kirsche visualization -sc $3 -th $4
+echo "Finished"
+
